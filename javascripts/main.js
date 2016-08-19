4d392e63871e22247ff0cb8b11037e87623b241c
Eduardo Lopez <eddielopez2101@gmail.com>
2016-08-19 14:09:28 -0500

    interaction for adding todos complete

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,4 +7,32 @@
 // jQuery's version of "DOMContentLoaded"
 $(function(){
   // All DOM related code can go here
+
+  var $todoFormEl = $("[data-js='todoForm']");
+  var $newTodoEl = $("[data-js='newTodo']");
+  var $addTodo = $("[data-js='submit']");
+  var $todoList = $("[data-js='todo__Items']");
+  console.log($todoFormEl, $newTodoEl, $addTodo, $todoList);
+
+  $addTodo.on("click", function(e) {
+    e.preventDefault();
+    var $todoValue = $newTodoEl.val();
+    if ($todoValue.length > 2){
+      addTodo($todoValue);
+      $newTodoEl.val("");
+    };
+  });
+
+  function addTodo(todoString){
+    var todoTemplate = `
+      <li class="todo__item">
+        ${todoString}
+      </li>
+    `;
+    $todoList.append(todoTemplate);
+  }
+
+
+
+
 });
